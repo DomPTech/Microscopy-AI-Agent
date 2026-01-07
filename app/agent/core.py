40fe6c4a1c4886ab151e455e2e50133f9a84d1cd
DomPTech <135615952+DomPTech@users.noreply.github.com>
2026-01-07 14:38:31 -0500

    feat: Implemented basic smol Agent

--- app/agent/core.py
+++ app/agent/core.py
@@ -1,9 +1,17 @@
+from smolagents import CodeAgent, WebSearchTool, TransformersModel
+
 class Agent:
     def __init__(self):
-        pass
+        self.model = model = TransformersModel(
+            model_id="Qwen/Qwen3-Next-80B-A3B-Thinking",
+            max_new_tokens=4096,
+            device_map="auto"
+        )
+        self.agent = CodeAgent(tools=[WebSearchTool()], model=model, stream_outputs=True)
 
-    def chat(self, user_input: str) -> str:
+    def chat(self, query: str) -> str:
         """
         Process user input and return a response.
         """
+        self.agent.run(query)
         return "This is a dummy agent response."
