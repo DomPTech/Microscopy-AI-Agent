438d436a1a3872040d802d6d73fa961fa2fa35c4
DomPTech <135615952+DomPTech@users.noreply.github.com>
2026-01-08 17:43:04 +0000

    feat: Implemented quantization for model

--- app/agent/core.py
+++ app/agent/core.py
@@ -1,11 +1,18 @@
 from smolagents import CodeAgent, WebSearchTool, TransformersModel
+from transformers import BitsAndBytesConfig
 
 class Agent:
     def __init__(self):
+        quant_config = BitsAndBytesConfig(
+            load_in_4bit=True,
+            bnb_4bit_compute_dtype="bfloat16"
+        )
+
         self.model = model = TransformersModel(
             model_id="Qwen/Qwen3-Next-80B-A3B-Thinking",
             max_new_tokens=4096,
-            device_map="auto"
+            device_map="auto",
+            model_kwargs={"quantization_config": quant_config}
         )
         self.agent = CodeAgent(tools=[WebSearchTool()], model=model, stream_outputs=True)
 
